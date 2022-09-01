6b0f771301f1bae8d26f223231fc58184adab17b
Fahmidur-arnob <arnobdev93@gmail.com>
2022-09-01 19:59:17 +0600

    Fixed the navbar

--- js/mealDB.js
+++ js/mealDB.js
@@ -14,9 +14,19 @@ const displayMeals = meals => {
 
     //for each loop to get each meals;
     meals.forEach(meal => {
+        const mealDiv = document.createElement('div');
+        mealDiv.innerHTML = `
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
         
+        `;
     });
 }
-
 
 loadMeals();
