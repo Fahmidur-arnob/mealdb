4fa48900888b7be9e64fa28ac6aeae63deb7ebb9
Fahmidur-arnob <arnobdev93@gmail.com>
2022-09-01 19:54:09 +0600

    added bootstrap cards, also found some bugs

--- js/mealDB.js
+++ js/mealDB.js
@@ -5,7 +5,18 @@ const loadMeals = () => {
     
     fetch(url)//this line fetches the url;
     .then(res => res.json())//this line converts the response to json
-    .then(data => console.log(data.meals))//this line just prints the json data;
+    .then(data => displayMeals(data.meals))//this line just prints the json data;
 }
+
+// this function will display what various meals we have 
+const displayMeals = meals => {
+    const mealContainer = document.getElementById('mealContainer');
+
+    //for each loop to get each meals;
+    meals.forEach(meal => {
+        
+    });
+}
+
 
 loadMeals();
